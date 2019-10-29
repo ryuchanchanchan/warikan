fddf3cf8eb2781f2be8c70b4d2195f81602ba45b
Ryu <kuromamekikaku@gmail.com>
2019-10-29 16:07:49 +0900

    関数の定義の方法を学んだ

---
+++ app.py
@@ -0,0 +1,31 @@
+# def hello():
+#     print("Hello World")
+#
+#
+# hello()
+#
+#
+# def say_hello(name):
+#     print(f"Hi {name}")
+#
+#
+# say_hello("Bob")
+# say_hello("Tom")
+
+def double(number):
+    return 2 * number
+
+
+result_1 = double(3)
+
+print(result_1)
+
+
+# 1分課題
+
+def str_convine(str1, str2):
+    return f"{str1}{str2}"
+
+
+result = str_convine("Ryu", "Kawamura")
+print(result)
